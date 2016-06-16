1d0aa53226eefae1f0a6c456fdedb9d6c2e815a4
Alberto Lopez <albertolopez19@gmail.com>
2016-06-16 15:09:09 -0400

    Super basic form validation

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import webapp2
+import cgi
 
 
 form = """
 <form method="post">
     What is your birthday?
-    <label>
-        Month
-        <input type="text" name="month">
-    </label>
-    <label>
-        Day
-        <input type="text" name="day">
-    </label>
-    <label>
-        Year
-        <input type="text" name="year">
-    </label>
+    <br>
+    <label>Month <input type="text" name="month" value="%(month)s"></label>
+    <label>Day <input type="text" name="day" value="%(day)s"></label>
+    <label>Year <input type="text" name="year" value="%(year)s"></label>
+    <div style="color: red">%(error)s</div>
     <br>
     <br>
     <input type="submit">
@@ -38,27 +32,48 @@ months = ['January',
 months_abbrvs = dict((m[:3].lower(), m) for m in months)
 
 class MainPage(webapp2.RequestHandler):
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error,
+                                        "month": self.escape_html(month),
+                                        "day": self.escape_html(day),
+                                        "year": self.escape_html(year)})
+
     def get(self):
-        self.response.write(form)
+        self.write_form(form)
 
     def post(self):
-        self.response.out.write("Thanks!")
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
 
-    def valid_month(month):
+        month = self.valid_month(user_month)
+        day = self.valid_day(user_day)
+        year = self.valid_year(user_year)
+
+        if not (month and day and year):
+            self.write_form("Please enter valid text.",
+                            user_month, user_day, user_year)
+        else:
+            self.response.out.write("Thanks!")
+
+    def valid_month(self, month):
         if month:
             short_month = month[:3].lower()
             return months_abbrvs.get(short_month)
 
-    def valid_day(day):
+    def valid_day(self, day):
         if day and day.isdigit():
             int_day = int(day)
             if 1 <= int_day <= 31:
                 return day
 
-    def valid_year(year):
+    def valid_year(self, year):
         if year and year.isdigit():
             user_input = int(year)
             if 1900 <= user_input <= 2020:
                 return int(year)
+
+    def escape_html(self, s):
+        return cgi.escape(s, quote=True)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
